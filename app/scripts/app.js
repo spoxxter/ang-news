ddfde014a66116e80335d81e4e8d4d7787b54c73
Jason Sisi <jason.sisi@gmail.com>
2015-02-14 14:12:13 -0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+/* global app:true */
+/* exported app */
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+var app = angular
+  .module('angNewsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+  .constant('FIREBASE_URL', 'https://scorching-fire-2863.firebaseio.com/');
